03602fe7295caa737d88d72b63e92812e94b0ba9
Abel Herrera <ahpard97@gmail.com>
2023-01-30 01:32:51 -0600

    add handle  input change

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('works');
+import { store } from './src/store.js';
+
+const ID_MAPS = {
+   INPUT_NAME: 'name',
+};
+
+const inputName = document.getElementById(ID_MAPS.INPUT_NAME);
+
+inputName.onkeyup = function (e) {
+   const { name, value } = e.target;
+
+   store.setFormValues(name, value);
+};
